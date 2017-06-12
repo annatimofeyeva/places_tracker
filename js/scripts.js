62091cd1a607e43b3f37a80b41af66366935aa91
Anna Kuznetsova and Anna Timofeeva <anna+anna@>
2017-06-12 16:00:11 -0700

    js user interface issues fixed

--- js/scripts.js
+++ js/scripts.js
@@ -16,24 +16,52 @@ function resetFields() {
   $("#new-city-name").val("");
   $("#new-country-name").val("");
   $("#new-season-name").val("");
-  $("#add-thing-name").val("");
+  $("#new-thing").val("");
 }
 
 //user interface logic
 
-$(document).ready.(function(){
-  $("#go_button").submit(function(event){
+$(document).ready(function(){
+
+  $("#add-thing-name").click(function() {
+      $(".new-thing").append('<div class="">' +
+                                   '<div class="form-group new-thing">' +
+                                     '<label for="new-thing">Things to do</label>' +
+                                     '<input type="text" class="form-control" id="new-thing">' +
+                                   '</div>');
+});
+  $("form#new-location").submit(function(event){
   event.preventDefault();
 
-  var inputtedCityName= ("input#new-city-name").val();
-  var inputtedCountryName = ("input#new-country-name").val();
-  var inputtedSeasonName = ("input#new-season-name").val();
-  var inputtedThingName = ("input#add-thing-name").val();
+  var inputtedSeasonName = $("#new-season-name").val();
+  var inputtedThingName = $("input#add-thing-name").val();
 
   var newTracker = new Tracker(inputtedSeasonName, inputtedThingName);
+  $(".city_country").each(function(){
+  var inputtedCityName = $("input#new-city-name").val();
+  var inputtedCountryName = $("input#new-country-name").val();
+  var newDestination = new Destination (inputtedCityName, inputtedCountryName);
+  console.log(newDestination);
+  newTracker.locationName.push(newDestination);
+  console.log(newTracker.locationName);
+});
 
-  }
+$("ul#list").append("<li><span class='contact'>" + newDestination.fullDestination() + "</span></li>");
 
 
+$(".contact").last().click(function() {
+      $("#show-contact").show();
+      $("#show-contact h3").text(newTracker.fullDestination());
+      $("#new-city-name").text(newDestination.cityName);
+      $("#new-country-name").text(newDestination.countryName);
+      $("#new-season-name").text(newTracker.seasonName);
+      $("ul#new-thing").text("");
+      newContact.addresses.forEach(function(address) {
+        $("ul#places").append("<li>" + newTracker.thingsName + "</li>");
+      });
+    });
 
-}
+    resetFields();
+
+  });
+});
